46a496faab48c5cc2a1b4b1dd96af2cd317dbe20
ozzwoy <dima.pashenko.200081@gmail.com>
2020-10-17 03:03:55 +0300

    Created f_func and g_func templates, linked Boost library to project

---
+++ lab1/f.cpp
@@ -0,0 +1,7 @@
+#include "demofuncs.h"
+#include <iostream>
+
+int main(int test_case) {
+	std::cout << spos::lab1::demo::f_func< spos::lab1::demo::op_group::INT >(test_case);
+	return 0;
+}
---
+++ lab1/g.cpp
@@ -0,0 +1,7 @@
+#include "demofuncs.h"
+#include <iostream>
+
+int main(int test_case) {
+	std::cout << spos::lab1::demo::g_func< spos::lab1::demo::op_group::INT >(test_case);
+	return 0;
+}
