57b0487d66080c3fa9746d394fc3e2b329be54de
ozzwoy <dima.pashenko.200081@gmail.com>
2020-10-18 02:30:20 +0300

    Added build instructions for f & g

--- lab1/f_process/f.cpp
+++ lab1/f_process/f.cpp
@@ -1,7 +1,9 @@
-#include "demofuncs.h"
+#include "../demofuncs/demofuncs.h"
 #include <iostream>
 
-int main(int test_case) {
+int main() {
+    int test_case;
+    std::cin >> test_case;
 	std::cout << spos::lab1::demo::f_func< spos::lab1::demo::op_group::INT >(test_case);
 	return 0;
 }
--- lab1/g_process/g.cpp
+++ lab1/g_process/g.cpp
@@ -1,7 +1,9 @@
-#include "demofuncs.h"
+#include "../demofuncs/demofuncs.h"
 #include <iostream>
 
-int main(int test_case) {
+int main() {
+    int test_case;
+    std::cin >> test_case;
 	std::cout << spos::lab1::demo::g_func< spos::lab1::demo::op_group::INT >(test_case);
 	return 0;
 }
